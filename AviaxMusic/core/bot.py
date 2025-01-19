9ef2c58e29e15173de9b6e17e3da06a68e3f2abb
Anjlnobita <jiteshkumar7200@gmail.com>
2025-01-20 03:36:37 +0530

    Update bot.py

--- AviaxMusic/core/bot.py
+++ AviaxMusic/core/bot.py
@@ -54,3 +54,27 @@ class Aviax(Client):
 
     async def stop(self):
         await super().stop()
+
+
+
+
+class nobita(Client):
+    def __init__(self):
+        super().__init__(
+            name="shizuchat",
+            api_id=config.API_ID,
+            api_hash=config.API_HASH,
+            bot_token=config.BOT_TOKEN,
+            in_memory=True,
+            parse_mode=ParseMode.DEFAULT,
+        )
+
+    async def start(self):
+        await super().start()
+        self.id = self.me.id
+        self.name = self.me.first_name + " " + (self.me.last_name or "")
+        self.username = self.me.username
+        self.mention = self.me.mention
+
+    async def stop(self):
+        await super().stop()
